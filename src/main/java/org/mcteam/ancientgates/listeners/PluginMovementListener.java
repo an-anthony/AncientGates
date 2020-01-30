313423cbf624c2ed6a11af9bdfba92cb018e5b96
an_anthony <3415838844@qq.com>
2020-01-31 00:28:59 +0800

    太吵了！优化！

--- src/main/java/org/mcteam/ancientgates/listeners/PluginMovementListener.java
+++ src/main/java/org/mcteam/ancientgates/listeners/PluginMovementListener.java
@@ -79,8 +79,10 @@ public class PluginMovementListener implements Listener {
 					coolTime = (lastTeleportTime + coolTime * 1000 - now) / 1000;
 					if(coolTime > 0){
 						//需要冷却
-						player.sendMessage("§9GLaDos §f>> §b" + String.format("你恐怕还得等上%d秒才能再次进入这个测试。",coolTime));
-						Plugin.lastMessageTime.put(player.getName(), now);
+						if(Plugin.lastMessageTime.get(player.getName()) < now - 10000L){
+							player.sendMessage("§9GLaDos §f>> §b" + String.format("你恐怕还得等上%d秒才能再次进入这个测试。",coolTime));
+							Plugin.lastMessageTime.put(player.getName(), now);
+						}
 						return;
 					}
 				}
